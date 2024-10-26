ad0f1c6507adb9d431e8749d01e73e6a393c4542
dheerajuparkoti <dheeraj.uparkoti.17@gmail.com>
2024-10-26 19:21:16 +0545

    removed blood donation folder

--- blood_donation/lib/data/internet_connectivity.dart
+++
@@ -1,8 +0,0 @@
-import 'package:connectivity/connectivity.dart';
-
-class ConnectivityUtil {
-  static Future<bool> isInternetConnected() async {
-    final connectivityResult = await Connectivity().checkConnectivity();
-    return connectivityResult != ConnectivityResult.none;
-  }
-}
